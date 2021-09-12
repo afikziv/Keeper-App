158c11d4c16da7ba97c73f13757d9efb2e98500f
afik <afikziv@gmail.com>
2021-09-12 12:17:43 +0300

    updated style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import AppleIcon from "@material-ui/icons/Apple";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>
+        <AppleIcon />
+        Keeper
+      </h1>
     </header>
   );
 }
